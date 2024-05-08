14821ae7735588d6fef993f5f541fff22684d8e3
aliensen36 <142628769+aliensen36@users.noreply.github.com>
2024-05-08 15:39:20 +0300

    Edit functions

--- Personal_financial_wallet.py
+++ Personal_financial_wallet.py
@@ -1,158 +1,178 @@
 import os
 
-# Вывод текущего баланса
-def show_balance(records):
-    total_income = sum(record['amount'] for record in records if record['category'] == 'Доход')
-    total_expense = sum(record['amount'] for record in records if record['category'] == 'Расход')
-    total_balance = total_income - total_expense
-    print(f"Текущий баланс: {total_balance}")
-    print(f"Доходы: {total_income}")
-    print(f"Расходы: {total_expense}")
+# Класс записей в Финансовом кошельке
+class Record:
+    def __init__(self, date, category, amount, description):
+        self.date = date
+        self.category = category
+        self.amount = float(amount)
+        self.description = description
 
-# Добавление записи
-def add_record(records):
-    print("Введите данные о новой записи:")
-    date = input("Дата (гггг-мм-дд): ")
-    category_code = input("Выберите категорию (1 - Доход, 2 - Расход): ")
-    category = 'Доход' if category_code == '1' else 'Расход'
-    amount = float(input("Сумма: "))
-    description = input("Описание: ")
-    records.append({'date': date, 'category': category, 'amount': amount, 'description': description})
-    print("Запись успешно добавлена!")
+# Класс Финансовый кошелек
+class FinancialWallet:
+    def __init__(self, filename):
+        self.filename = filename
+        self.records = self.load_records_from_file()
 
-# Редактирование записи
-def edit_record(records):
-    print("Введите номер записи, которую хотите отредактировать:")
-    for i, record in enumerate(records):
-        print(f"{i + 1}. {record['date']} - {record['category']} - {record['amount']} - {record['description']}")
-    try:
-        index = int(input("Номер записи: ")) - 1
-        if 0 <= index < len(records):
-            print("Выберите поле для редактирования:")
-            print("1. Дата")
-            print("2. Категория")
-            print("3. Сумма")
-            print("4. Описание")
-            choice = input("Введите номер поля: ")
-            if choice == '1':
-                records[index]['date'] = input("Введите новую дату (гггг-мм-дд): ")
-            elif choice == '2':
-                category_code = input("Выберите новую категорию (1 - Доход, 2 - Расход): ")
-                records[index]['category'] = 'Доход' if category_code == '1' else 'Расход'
-            elif choice == '3':
-                records[index]['amount'] = float(input("Введите новую сумму: "))
-            elif choice == '4':
-                records[index]['description'] = input("Введите новое описание: ")
-            else:
-                print("Некорректный ввод.")
-        else:
-            print("Некорректный номер записи.")
-    except ValueError:
-        print("Некорректный ввод.")
+    # Функция загрузки записей из файла
+    def load_records_from_file(self):
+        records = []
+        if os.path.exists(self.filename):
+            with open(self.filename, 'r') as file:
+                lines = file.readlines()
+                for line in lines:
+                    line = line.strip()
+                    if line:
+                        record_data = self.parse_record(line)
+                        records.append(Record(**record_data))
+        return records
 
+    def parse_record(self, line):
+        data = {}
+        parts = line.split(', ')
+        for part in parts:
+            key, value = part.split(': ')
+            key = key.strip().lower()
+            value = value.strip()
+            if key == 'дата':
+                key = 'date'
+            elif key == 'категория':
+                key = 'category'
+            elif key == 'сумма':
+                key = 'amount'
+            elif key == 'описание':
+                key = 'description'
+            data[key] = value
+        return data
 
-# Поиск записи
-def search_records(records):
-    print("Выберите критерии для поиска:")
-    print("1. По категории")
-    print("2. По дате")
-    print("3. По сумме")
-    choice = input("Введите номер критерия: ")
-    if choice == '1':
+    # Функция сохранения записей в файл
+    def save_records_to_file(self):
+        with open(self.filename, 'w') as file:
+            for record in self.records:
+                file.write(
+                    f"Дата: {record.date}, Категория: {record.category}, Сумма: {record.amount}, Описание: {record.description}\n")
+        print("Данные сохранены в файл.")
+
+    # Показ текущего баланса
+    def show_balance(self):
+        total_income = sum(record.amount for record in self.records if record.category == 'Доход')
+        total_expense = sum(record.amount for record in self.records if record.category == 'Расход')
+        total_balance = total_income - total_expense
+        print(f"Текущий баланс: {total_balance}")
+        print(f"Доходы: {total_income}")
+        print(f"Расходы: {total_expense}")
+
+    # Функция добавления записей
+    def add_record(self):
+        print("Введите данные о новой записи:")
+        date = input("Дата (гггг-мм-дд): ")
         category_code = input("Выберите категорию (1 - Доход, 2 - Расход): ")
         category = 'Доход' if category_code == '1' else 'Расход'
-        found_records = [record for record in records if record['category'] == category]
-    elif choice == '2':
-        date = input("Введите дату (гггг-мм-дд): ")
-        found_records = [record for record in records if record['date'] == date]
-    elif choice == '3':
-        amount = float(input("Введите сумму: "))
-        found_records = [record for record in records if record['amount'] == amount]
-    else:
-        print("Некорректный ввод.")
-        return
+        amount = float(input("Сумма: "))
+        description = input("Описание: ")
+        new_record = Record(date, category, amount, description)
+        self.records.append(new_record)
+        print("Запись успешно добавлена!")
 
-    if found_records:
-        print("Найденные записи:")
-        for record in found_records:
-            print(
-                f"Дата: {record['date']}, Категория: {record['category']}, Сумма: {record['amount']}, Описание: {record['description']}")
-    else:
-        print("Записи не найдены.")
-
-
-# Сохранение записей в файл
-def save_records_to_file(records, filename):
-    with open(filename, 'w') as file:
-        for record in records:
-            file.write(f"Дата: {record['date']}, Категория: {record['category']}, Сумма: {record['amount']}, Описание: {record['description']}\n")
-    print("Данные сохранены в файл.")
-
-# Показ записей из файла
-def show_file_content(filename):
-    if os.path.exists(filename):
-        with open(filename, "r") as file:
-            print("Содержимое файла:")
-            for line in file:
-                print(line.strip())
-    else:
-        print("Файл не найден.")
-
-
-# Загрузка записей из файла
-def load_records_from_file(filename):
-    records = []
-    if os.path.exists(filename):
-        with open(filename, 'r') as file:
-            lines = file.readlines()
-            record = {}
-            for line in lines:
-                line = line.strip()
-                if line:
-                    key, value = line.split(':', 1)  # Ограничение до одного разбиения
-                    record[key.strip()] = value.strip()  # Убираем лишние пробелы
+    # Функция редактирования записей
+    def edit_record(self):
+        print("Введите номер записи, которую хотите отредактировать:")
+        for i, record in enumerate(self.records):
+            print(f"{i + 1}. {record.date} - {record.category} - {record.amount} - {record.description}")
+        try:
+            index = int(input("Номер записи: ")) - 1
+            if 0 <= index < len(self.records):
+                print("Выберите поле для редактирования:")
+                print("1. Дата")
+                print("2. Категория")
+                print("3. Сумма")
+                print("4. Описание")
+                choice = input("Введите номер поля: ")
+                if choice == '1':
+                    self.records[index].date = input("Введите новую дату (гггг-мм-дд): ")
+                elif choice == '2':
+                    category_code = input("Выберите новую категорию (1 - Доход, 2 - Расход): ")
+                    self.records[index].category = 'Доход' if category_code == '1' else 'Расход'
+                elif choice == '3':
+                    self.records[index].amount = float(input("Введите новую сумму: "))
+                elif choice == '4':
+                    self.records[index].description = input("Введите новое описание: ")
                 else:
-                    records.append(record)
-                    record = {}
-            if record:
-                records.append(record)
-    return records
+                    print("Некорректный ввод.")
+            else:
+                print("Некорректный номер записи.")
+        except ValueError:
+            print("Некорректный ввод.")
 
-
-# Имя файла для хранения данных
-filename = "financial_records.txt"
-
-
-def main():
-    records = load_records_from_file(filename)
-    while True:
-        print("\nЛичный финансовый кошелек")
-        print("1. Просмотр баланса")
-        print("2. Добавить запись")
-        print("3. Редактировать запись")
-        print("4. Поиск записей")
-        print("5. Сохранить записи в файл")
-        print("6. Показать содержимое файла")
-        print("0. Выйти из программы")
-        choice = input("Выберите действие: ")
-
-        if choice == "1":
-            show_balance(records)
-        elif choice == "2":
-            add_record(records)
-        elif choice == "3":
-            edit_record(records)
-        elif choice == "4":
-            search_records(records)
-        elif choice == "5":
-            save_records_to_file(records, filename)
-        elif choice == "6":
-            show_file_content(filename)
-        elif choice == "0":
-            break
+    # Функция поиска записей
+    def search_records(self):
+        print("Выберите критерии для поиска:")
+        print("1. По категории")
+        print("2. По дате")
+        print("3. По сумме")
+        choice = input("Введите номер критерия: ")
+        if choice == '1':
+            category_code = input("Выберите категорию (1 - Доход, 2 - Расход): ")
+            category = 'Доход' if category_code == '1' else 'Расход'
+            found_records = [record for record in self.records if record.category == category]
+        elif choice == '2':
+            date = input("Введите дату (гггг-мм-дд): ")
+            found_records = [record for record in self.records if record.date == date]
+        elif choice == '3':
+            amount = float(input("Введите сумму: "))
+            found_records = [record for record in self.records if record.amount == amount]
         else:
-            print("Неверный ввод. Пожалуйста, выберите действие из списка.")
+            print("Некорректный ввод.")
+            return
 
+        if found_records:
+            print("Найденные записи:")
+            for record in found_records:
+                print(
+                    f"Дата: {record.date}, Категория: {record.category}, Сумма: {record.amount}, Описание: {record.description}")
+        else:
+            print("Записи не найдены.")
+
+    # Функция показа записей из файла
+    def show_file_content(self):
+        if os.path.exists(self.filename):
+            with open(self.filename, "r") as file:
+                print("Содержимое файла:")
+                for line in file:
+                    print(line.strip())
+        else:
+            print("Файл не найден.")
+
+    # Основная функция программы
+    def main(self):
+        while True:
+            print("\nЛичный финансовый кошелек")
+            print("1. Просмотр баланса")
+            print("2. Добавить запись")
+            print("3. Редактировать запись")
+            print("4. Поиск записей")
+            print("5. Сохранить записи в файл")
+            print("6. Показать содержимое файла")
+            print("0. Выйти из программы")
+            choice = input("Выберите действие: ")
+
+            if choice == "1":
+                self.show_balance()
+            elif choice == "2":
+                self.add_record()
+            elif choice == "3":
+                self.edit_record()
+            elif choice == "4":
+                self.search_records()
+            elif choice == "5":
+                self.save_records_to_file()
+            elif choice == "6":
+                self.show_file_content()
+            elif choice == "0":
+                break
+            else:
+                print("Неверный ввод. Пожалуйста, выберите действие из списка.")
 
 if __name__ == "__main__":
-    main()
+    wallet = FinancialWallet("financial_records.txt")
+    wallet.main()
--- README.md
+++ README.md
@@ -19,109 +19,47 @@
 
 **Документация**
 
-def show_balance(records):
-    """
-    Выводит текущий баланс, доходы и расходы на основе списка записей.
+Финансовый кошелек
 
-    Parameters:
-    records (list): Список записей о доходах и расходах.
+Этот скрипт реализует простую программу управления личными финансами. Он включает в себя два основных класса: Record и FinancialWallet.
 
-    Returns:
-    None
-    """
-    total_income = sum(record['amount'] for record in records if record['category'] == 'Доход')
-    total_expense = sum(record['amount'] for record in records if record['category'] == 'Расход')
-    total_balance = total_income - total_expense
-    print(f"Текущий баланс: {total_balance}")
-    print(f"Доходы: {total_income}")
-    print(f"Расходы: {total_expense}")
+Класс Record представляет отдельную финансовую запись. У каждой записи есть дата, категория (доход или расход), сумма и описание.
 
-def add_record(records):
-    """
-    Добавляет новую запись о доходе или расходе в список записей.
+Класс FinancialWallet представляет собой финансовый кошелек. Он имеет методы для загрузки записей из файла, сохранения записей в файл, просмотра текущего баланса, добавления новых записей, редактирования существующих записей, поиска записей по различным критериям и отображения содержимого файла.
 
-    Parameters:
-    records (list): Список записей о доходах и расходах.
+Каждый кошелек представлен файлом, имя которого передается в качестве аргумента конструктору класса FinancialWallet.
 
-    Returns:
-    None
-    """
-    print("Введите данные о новой записи:")
-    date = input("Дата (гггг-мм-дд): ")
-    category_code = input("Выберите категорию (1 - Доход, 2 - Расход): ")
-    category = 'Доход' if category_code == '1' else 'Расход'
-    amount = float(input("Сумма: "))
-    description = input("Описание: ")
-    records.append({'date': date, 'category': category, 'amount': amount, 'description': description})
-    print("Запись успешно добавлена!")
+Документация по методам:
 
-def edit_record(records):
-    """
-    Редактирует существующую запись о доходе или расходе в списке записей.
+1. load_records_from_file():
+   - Загружает записи из файла и возвращает список объектов Record.
 
-    Parameters:
-    records (list): Список записей о доходах и расходах.
+2. parse_record(line):
+   - Разбирает строку с данными о записи и возвращает словарь с ключами и значениями.
 
-    Returns:
-    None
-    """
-    print("Введите номер записи, которую хотите отредактировать:")
-    for i, record in enumerate(records):
-        print(f"{i + 1}. {record['date']} - {record['category']} - {record['amount']} - {record['description']}")
-    try:
-        index = int(input("Номер записи: ")) - 1
-        if 0 <= index < len(records):
-            print("Выберите поле для редактирования:")
-            print("1. Дата")
-            print("2. Категория")
-            print("3. Сумма")
-            print("4. Описание")
-            choice = input("Введите номер поля: ")
-            if choice == '1':
-                records[index]['date'] = input("Введите новую дату (гггг-мм-дд): ")
-            elif choice == '2':
-                category_code = input("Выберите новую категорию (1 - Доход, 2 - Расход): ")
-                records[index]['category'] = 'Доход' if category_code == '1' else 'Расход'
-            elif choice == '3':
-                records[index]['amount'] = float(input("Введите новую сумму: "))
-            elif choice == '4':
-                records[index]['description'] = input("Введите новое описание: ")
-            else:
-                print("Некорректный ввод.")
-        else:
-            print("Некорректный номер записи.")
-    except ValueError:
-        print("Некорректный ввод.")
+3. save_records_to_file():
+   - Сохраняет текущие записи в файл.
 
-def search_records(records):
-    """
-    Ищет записи о доходах или расходах в списке записей по заданным критериям.
+4. show_balance():
+   - Выводит текущий баланс, доходы и расходы на экран.
 
-    Parameters:
-    records (list): Список записей о доходах и расходах.
+5. add_record():
+   - Запрашивает у пользователя данные о новой записи и добавляет их в список записей.
 
-    Returns:
-    None
-    """
-    print("Выберите критерии для поиска:")
-    print("1. По категории")
-    print("2. По дате")
-    print("3. По сумме")
-    choice = input("Введите номер критерия: ")
-    if choice == '1':
-        category_code = input("Выберите категорию (1 - Доход, 2 - Расход): ")
-        category = 'Доход' if category_code == '1' else 'Расход'
-        found_records = [record for record in records if record['category'] == category]
-    elif choice == '2':
-        date = input("Введите дату (гггг-мм-дд): ")
-        found_records = [record for record in records if record['date'] == date]
-    elif choice == '3':
-        amount = float(input("Введите сумму: "))
-        found_records = [record for record in records if record['amount'] == amount]
-    else:
-        print("Некорректный ввод.")
-        return
+6. edit_record():
+   - Позволяет пользователю выбрать запись для редактирования и изменить ее дату, категорию, сумму или описание.
 
-    if found_records:
-        print("Найденные записи:")
-        for record in found_records:
+7. search_records():
+   - Позволяет пользователю выбрать критерий для поиска записей: по категории, дате или сумме.
+
+8. show_file_content():
+   - Выводит содержимое файла на экран.
+
+9. main():
+   - Основной метод программы, в котором пользователь может выбирать действия из списка.
+
+Пример использования:
+    wallet = FinancialWallet("financial_records.txt")
+    wallet.main()
+
+Это создаст объект FinancialWallet для управления финансовыми записями в файле financial_records.txt и запустит основной цикл программы.
